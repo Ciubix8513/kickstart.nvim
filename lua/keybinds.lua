cc51c7e56ce75d31a4e6fdee8fcfbbf604db819b
Luna <ciubix8514@gmail.com>
2025-12-27 00:20:56 +0400

    Keybinds :3

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,20 +1,19 @@
 local M = {}
 
-
 local lazygit_toggle = function()
-  local Terminal = require("toggleterm.terminal").Terminal
+  local Terminal = require('toggleterm.terminal').Terminal
   local lazygit = Terminal:new {
-    cmd = "lazygit",
+    cmd = 'lazygit',
     hidden = true,
-    direction = "float",
+    direction = 'float',
     float_opts = {
-      border = "none",
+      border = 'none',
       width = 100000,
       height = 100000,
       zindex = 200,
     },
     on_open = function(_)
-      vim.cmd "startinsert!"
+      vim.cmd 'startinsert!'
     end,
     on_close = function(_) end,
     count = 99,
@@ -25,10 +24,14 @@ end
 function M.add_keybinds()
   local normal_mode = {
     -- Movement between windows
-    ["<C-h>"] = ":wincmd h<CR>",
-    ["<C-j>"] = ":wincmd j<CR>",
-    ["<C-k>"] = ":wincmd k<CR>",
-    ["<C-l>"] = ":wincmd l<CR>",
+    ['<C-h>'] = ':wincmd h<CR>',
+    ['<C-j>'] = ':wincmd j<CR>',
+    ['<C-k>'] = ':wincmd k<CR>',
+    ['<C-l>'] = ':wincmd l<CR>',
+    ['<C-left>'] = ':vertical:res -2<CR>',
+    ['<C-right>'] = ':vertical:res +2<CR>',
+    ['<C-up>'] = ':res -2<CR>',
+    ['<C-down>'] = ':res +2<CR>',
     -- ["р"] = "h",
     -- ["о"] = "j",
     -- ["л"] = "k",
@@ -53,12 +56,12 @@ function M.add_keybinds()
     -- ["М"] = "V",
     -- ["ч"] = "x",
     -- ["Ч"] = "X",
-    ["<Leader>gg"] = { func = lazygit_toggle, desc = "open lazygit" },
-    ["<Leader>h"] = { func = ":noh<CR>", desc = "Clear search"},
+    ['<Leader>gg'] = { func = lazygit_toggle, desc = 'open lazygit' },
+    ['<Leader>h'] = { func = ':noh<CR>', desc = 'Clear search' },
   }
   local visual_mode = {
-    ["<"] = "<gv",
-    [">"] = ">gv",
+    ['<'] = '<gv',
+    ['>'] = '>gv',
     -- ["р"] = "h",
     -- ["о"] = "j",
     -- ["л"] = "k",
@@ -85,16 +88,16 @@ function M.add_keybinds()
   local opts = { noremap = true, silent = true }
 
   for key, cmd in pairs(normal_mode) do
-    if type(cmd) == "table" then
+    if type(cmd) == 'table' then
       local opts1 = { noremap = true, silent = true, desc = cmd.desc }
-      vim.keymap.set("n", key, cmd.func, opts1)
+      vim.keymap.set('n', key, cmd.func, opts1)
     else
-      vim.keymap.set("n", key, cmd, opts)
+      vim.keymap.set('n', key, cmd, opts)
     end
   end
 
   for key, cmd in pairs(visual_mode) do
-    vim.keymap.set("v", key, cmd, opts)
+    vim.keymap.set('v', key, cmd, opts)
   end
 end
 
